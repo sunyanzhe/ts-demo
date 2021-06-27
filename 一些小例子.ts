765c4da9e500fb6dc3ef2498cf8124452a174478
sunyanzhe <sunyanzhe@meituan.com>
2021-06-27 11:51:02 +0800

    feat: 联合类型变为交集

--- 一些小例子.ts
+++ 一些小例子.ts
@@ -23,3 +23,20 @@ const hdrs1: HttpHeadersInterface = {
 
 send({}, hdrs)
 // send({}, hdrs1)      // error no signature
+
+
+//   ------------------------------------------------ union to intersection ----------------------------------------------------------
+
+type Format320 = { urls: {format320p: string}}
+type Format480 = { urls: {format480p: string}}
+type Format720 = { urls: {format720p: string}}
+type Format1080 = { urls: {format1080p: string}}
+
+type Video = Format1080 | Format720 | Format480 | Format320
+
+type keys = Video['urls']
+
+// 由于第二个条件 推断infer R处于的位置为逆变位 为了兼容性 将 ｜ 转变为了 &
+type UnionToIntersection<T> = (T extends any ? (arg: T) => any : never) extends (arg: infer R) => any ? R : never
+
+type FormatKey = keyof UnionToIntersection<Video['urls']>
