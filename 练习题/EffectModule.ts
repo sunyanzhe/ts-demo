59b9889bd5dbb7d297d87786930dbdacecfc6d75
sunyanzhe <sunyanzhe@meituan.com>
2021-06-27 16:19:49 +0800

    练习题

---
+++ 练习题/EffectModule.ts
@@ -0,0 +1,40 @@
+interface Action<T> {
+  payload?: T;
+  type: string;
+}
+
+interface EffectModule {
+  count: number
+  message: string
+  asyncMethod<T, U>(input: Promise<T>): Promise<Action<U>>
+  syncMethod<T, U>(action: Action<T>): Action<U>
+}
+
+interface ResultModule {
+  asyncMethod<T, U>(input: T): Action<U>
+  syncMethod<T, U>(action: T): Action<U>
+}
+
+let aaa = {} as EffectModule
+
+type GetFunctionKeys<T> = {
+  [key in keyof T]: T[key] extends Function ? key : never
+}[keyof T]
+
+type FunctionMoudle = Pick<EffectModule, GetFunctionKeys<EffectModule>>
+
+type UnWarpAction<T> = T extends Action<infer P> ? P : T
+type UnWarpPromise<T> = T extends Promise<infer P> ? P : T
+
+type MapTypeToUnPromisifyAndUnAction<T extends any[]> = {
+  [key in keyof T]: UnWarpPromise<UnWarpAction<T[key]>>
+}
+
+type aa = MapTypeToUnPromisifyAndUnAction<[1,2,3,'a']>[number]
+
+type Connect = (o: EffectModule) => ({
+  [key in GetFunctionKeys<typeof o>]: (input: MapTypeToUnPromisifyAndUnAction<Parameters<EffectModule[key]>>[number]) => UnWarpAction<UnWarpPromise<ReturnType<EffectModule[key]>>>
+})
+
+declare const fn: Connect
+let reuslt = fn(aaa)
