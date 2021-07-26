857a8e9054f5cba1dbda294912c1f060a6d8f516
sunyanzhe <15900313259@163.com>
2021-07-26 20:50:57 +0800

    include type equal

--- 练习题/include.ts
+++ 练习题/include.ts
@@ -7,5 +7,21 @@ function execute(action: string) {
 }
 
 function includes<T extends U, U>(arrs: ReadonlyArray<T>, action: U): action is T {
-  return arrs.includes(U as T)
+  return arrs.includes(action as T)
 }
+
+type Trim<T extends string> = T extends `${infer A}${infer B}` 
+  ? B extends ''
+    ? A
+    : B
+  : never
+
+type t1 = Trim<'abb'>
+
+type ttt1 = 'a' | 'b' | 'c'
+type ttt2 = 'a' | 'c' | 'd'
+
+type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false
+type Includes<T extends any[], U> = true extends {
+  [K in keyof T]: MyEqual<T[K], U>
+}[number] ? true : false
