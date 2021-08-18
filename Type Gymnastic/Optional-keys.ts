cf24d0e42e85e99b5564e8b66b8d9e973eaa403d
sunyanzhe <15900313259@163.com>
2021-08-18 23:26:51 +0800

    optional-keys

---
+++ Type Gymnastic/Optional-keys.ts
@@ -0,0 +1,11 @@
+// https://github.com/type-challenges/type-challenges/blob/master/questions/90-hard-optional-keys/README.md
+
+// Implement the advanced util type OptionalKeys<T>, which picks all the optional keys into a union.
+
+// example 
+// type case = OptionalKeys<{ a: number, b?: string }>  "b"
+
+type OptionalKeys<T> = Exclude<keyof T, keyof RequiredKeys<T>>
+type RequiredKeys<T> = {
+  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K] 
+}
